a30224851739b147faeb8debbc8114d4f12849d0
Tobias Happ <tobias.happ@gmx.de>
2023-04-15 14:17:14 +0200

    feat: remove maintenance page

--- src/routes/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-
-export const load = (async ({ url }) => {
-    return {
-        showMaintenance: url.host === 'www.popchor-koeln.de' || url.host === 'popchor-koeln.de',
-    };
-}) satisfies LayoutServerLoad;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,18 @@
 <script lang="ts">
-    import { Construction } from 'lucide-svelte';
     import '../app.postcss';
-    import type { LayoutData } from './$types';
     import Footer from './Footer.svelte';
     import Header from './Header.svelte';
     import Navigation from './Navigation.svelte';
 
     let openNavigation: () => void;
-
-    export let data: LayoutData;
 </script>
 
-{#if data.showMaintenance}
-    <div class="fixed inset-0 flex flex-col items-center justify-center gap-6 font-heading">
-        <Construction />
-        <h1 class="text-2xl font-bold">Wartungsarbeiten</h1>
-    </div>
-{:else}
-    <Header {openNavigation} />
+<Header {openNavigation} />
 
-    <Navigation bind:openNavigation />
+<Navigation bind:openNavigation />
 
-    <main>
-        <slot />
-    </main>
+<main>
+    <slot />
+</main>
 
-    <Footer />
-{/if}
+<Footer />
