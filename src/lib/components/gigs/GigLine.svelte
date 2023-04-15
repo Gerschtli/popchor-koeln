ae56963a798e400ce94e9e4df5c9f82f6518aee2
Tobias Happ <tobias.happ@gmx.de>
2023-04-15 21:41:27 +0200

    fix: use grey color for gig line

--- src/lib/components/gigs/GigLine.svelte
+++ src/lib/components/gigs/GigLine.svelte
@@ -2,8 +2,8 @@
     export let label: string;
 </script>
 
-<div class="flex items-baseline">
-    <div class="mr-2 w-16 flex-shrink-0 text-right text-xs font-bold uppercase tracking-wide text-neutral-600">
+<div class="flex items-baseline text-neutral-600">
+    <div class="mr-2 w-16 flex-shrink-0 text-right text-xs font-bold uppercase tracking-wide">
         {label}:
     </div>
     <slot />
