8863c46781e9bd1e0598c95f02e86bab5383f153
Tobias Happ <tobias.happ@gmx.de>
2023-04-17 00:24:23 +0200

    refactor: use svelte transition for navigation

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -2,17 +2,15 @@
     import { browser } from '$app/environment';
     import { modalStore } from '$lib/modal';
     import { X } from 'lucide-svelte';
+    import { fly } from 'svelte/transition';
 
     let navigationModal = modalStore('navigation-open');
-    let tabIndexNav: number | null;
 
     $: if (browser) {
         $navigationModal.isOpen
             ? document.body.classList.add('overflow-hidden')
             : document.body.classList.remove('overflow-hidden');
     }
-
-    $: tabIndexNav = $navigationModal.isOpen ? null : -1;
 
     export const openNavigation = navigationModal.open;
 
@@ -27,28 +25,20 @@
 <svelte:window on:popstate={navigationModal.onPopstate} />
 <svelte:body on:keydown={navigationModal.onKeydown} />
 
-<nav
-    class="fixed inset-0 z-10 bg-white/90 transition-transform duration-200 ease-in-out"
-    class:-translate-y-full={!$navigationModal.isOpen}
->
-    <div class="container relative mx-auto p-8">
-        <ul class="flex flex-col gap-4 p-4">
-            {#each navigationItems as item}
-                <li>
-                    <a tabindex={tabIndexNav} class="text-lg hover:text-accent active:text-accent" href={item.href}
-                        >{item.text}</a
-                    >
-                </li>
-            {/each}
-        </ul>
+{#if $navigationModal.isOpen}
+    <nav class="fixed inset-0 z-10 bg-white/90" transition:fly={{ y: '100%', duration: 200 }}>
+        <div class="container relative mx-auto p-8">
+            <ul class="flex flex-col gap-4 p-4">
+                {#each navigationItems as item}
+                    <li>
+                        <a class="text-lg hover:text-accent active:text-accent" href={item.href}>{item.text}</a>
+                    </li>
+                {/each}
+            </ul>
 
-        <button
-            aria-label="Menü schließen"
-            tabindex={tabIndexNav}
-            class="absolute right-6 top-6 p-2"
-            on:click={navigationModal.close}
-        >
-            <X class="hover:text-slate-500" size={32} />
-        </button>
-    </div>
-</nav>
+            <button aria-label="Menü schließen" class="absolute right-6 top-6 p-2" on:click={navigationModal.close}>
+                <X class="hover:text-slate-500" size={32} />
+            </button>
+        </div>
+    </nav>
+{/if}
