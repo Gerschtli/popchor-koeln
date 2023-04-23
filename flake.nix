c4ff70ffa7ef96ea487b8c87ba50be71a8beff1b
Tobias Happ <tobias.happ@gmx.de>
2023-04-23 22:32:33 +0200

    chore: use pnpm from unstable

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   description = "popchor-koeln";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+  inputs.unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, unstable }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      pkgsUnstable = unstable.legacyPackages.${system};
     in
     {
       devShells.${system}.default = pkgs.mkShell {
@@ -14,7 +16,7 @@
           pkgs.jq
           pkgs.netlify-cli
           pkgs.nodejs_latest
-          pkgs.nodePackages_latest.pnpm
+          pkgsUnstable.nodePackages_latest.pnpm
         ];
 
         PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "true";
