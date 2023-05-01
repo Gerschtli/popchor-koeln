e04c2d6a7f928a0468baffc7bc2be2ecb6342e0d
Tobias Happ <tobias.happ@gmx.de>
2023-05-01 16:07:10 +0200

    chore: persist storyblok components config

--- src/lib/component-types-storyblok.d.ts
+++ src/lib/component-types-storyblok.d.ts
@@ -1,53 +1,53 @@
-import {StoryblokStory} from 'storyblok-generate-ts'
+import { StoryblokStory } from 'storyblok-generate-ts';
 
 export interface GigStoryblok {
-  title: string;
-  date: string;
-  entryTime?: string;
-  placeShort: string;
-  place: string;
-  price?: string;
-  linkTickets?: string;
-  description?: any;
-  _uid: string;
-  component: "gig";
-  [k: string]: any;
+    title: string;
+    date: string;
+    entryTime?: string;
+    placeShort: string;
+    place: string;
+    price?: string;
+    linkTickets?: string;
+    description?: any;
+    _uid: string;
+    component: 'gig';
+    [k: string]: any;
 }
 
 export interface GigsStoryblok {
-  list?: GigStoryblok[];
-  _uid: string;
-  component: "gigs";
-  [k: string]: any;
+    list?: GigStoryblok[];
+    _uid: string;
+    component: 'gigs';
+    [k: string]: any;
 }
 
 export interface GigsReferenceStoryblok {
-  showOnlyFuture?: boolean;
-  reference?: StoryblokStory<GigsStoryblok> | string;
-  _uid: string;
-  component: "gigs_reference";
-  [k: string]: any;
+    showOnlyFuture?: boolean;
+    reference?: StoryblokStory<GigsStoryblok> | string;
+    _uid: string;
+    component: 'gigs_reference';
+    [k: string]: any;
 }
 
 export interface PageStoryblok {
-  title: string;
-  description?: string;
-  body: SectionStoryblok[];
-  _uid: string;
-  component: "page";
-  uuid?: string;
-  [k: string]: any;
+    title: string;
+    description?: string;
+    body: SectionStoryblok[];
+    _uid: string;
+    component: 'page';
+    uuid?: string;
+    [k: string]: any;
 }
 
 export interface SectionStoryblok {
-  id?: string;
-  title: string;
-  gigs?: GigsReferenceStoryblok[];
-  content?: any;
-  showExpandButton?: boolean;
-  expandButtonText?: string;
-  contentExpand?: any;
-  _uid: string;
-  component: "section";
-  [k: string]: any;
+    id?: string;
+    title: string;
+    gigs?: GigsReferenceStoryblok[];
+    content?: any;
+    showExpandButton?: boolean;
+    expandButtonText?: string;
+    contentExpand?: any;
+    _uid: string;
+    component: 'section';
+    [k: string]: any;
 }
