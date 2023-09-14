26368245177538db428272e706f86f2fbf5eb3ad
Tobias Happ <tobias.happ@gmx.de>
2023-09-14 19:09:14 +0200

    feat: add spotify link

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
     import { Facebook, Instagram, Mail, Youtube } from 'lucide-svelte';
     import SocialLink from './SocialLink.svelte';
+    import SpotifyLogo from './SpotifyLogo.svelte';
 </script>
 
 <footer class="border-t border-slate-500">
-    <div class="container mx-auto flex items-center justify-between gap-y-4 p-8 sm:px-12 lg:px-20">
-        <div class="flex-1 sm:-order-1">
-            <a class="text-sm text-blue-500 hover:text-blue-700 focus:text-blue-700" href="/impressum"> Impressum </a>
-        </div>
-
+    <div class="container mx-auto flex flex-col items-center justify-between gap-y-4 p-8 sm:flex-row sm:px-12 lg:px-20">
         <div class="flex justify-center gap-4">
             <SocialLink label="Facebook" link="https://www.facebook.com/popchorkoeln/" icon={Facebook} />
             <SocialLink label="Email" link="mailto:hallo@popchor-koeln.de" icon={Mail} />
             <SocialLink label="Youtube" link="https://www.youtube.com/@popchorkoeln" icon={Youtube} />
             <SocialLink label="Instagram" link="https://www.instagram.com/popchorkoeln/" icon={Instagram} />
+            <SocialLink
+                label="Spotify"
+                link="https://open.spotify.com/intl-de/artist/19H5wDzTqtIbb0Y4CT9qCE?si=-fWY9Ht_QNmiaknfWNWCZQ"
+                icon={SpotifyLogo}
+            />
+        </div>
+
+        <div class="flex-1 sm:-order-1">
+            <a class="text-sm text-blue-500 hover:text-blue-700 focus:text-blue-700" href="/impressum">Impressum</a>
         </div>
 
         <div class="hidden sm:block sm:flex-1" />
