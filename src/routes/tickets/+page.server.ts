1231086cca3bbb89f229c3e212fa20a4d215e656
Tobias Happ <tobias.happ@gmx.de>
2023-09-17 18:49:18 +0200

    feat: point /tickets to /

--- src/routes/tickets/+page.server.ts
+++ src/routes/tickets/+page.server.ts
@@ -1,8 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export async function load() {
-    throw redirect(
-        307,
-        'https://www.eventbrite.com/e/popchorkoln-soundescape-open-air-doppelkonzert-tickets-620148610187',
-    );
+    throw redirect(303, '/');
 }
--- tests/tickets.spec.ts
+++ tests/tickets.spec.ts
@@ -1,19 +1,11 @@
 import { expect, test } from '@playwright/test';
 
 test.describe('/tickets', () => {
-    test('redirects to eventbrite', async ({ page }) => {
+    test('redirects to homepage', async ({ page }) => {
         await page.goto('/');
-
-        const navigationPromise = page.waitForNavigation({
-            url: /^https:\/\/www.eventbrite.com/,
-        });
 
         await page.goto('/tickets');
 
-        await navigationPromise;
-
-        await expect(page).toHaveURL(
-            'https://www.eventbrite.com/e/popchorkoln-soundescape-open-air-doppelkonzert-tickets-620148610187',
-        );
+        await expect(page).toHaveURL('/');
     });
 });
