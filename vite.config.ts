bc9ad104d1414ccc575d81fb21a88dc9197d1963
Tobias Happ <tobias.happ@gmx.de>
2023-09-17 15:38:39 +0200

    chore: enable ssl locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-    plugins: [sveltekit(), imagetools()],
+    plugins: [sveltekit(), imagetools(), basicSsl()],
+    server: { https: true },
 });
