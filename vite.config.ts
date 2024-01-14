ced93a5e72569a5b99ebca34880f60ff80b91a37
Tobias Happ <tobias.happ@gmx.de>
2024-01-14 17:58:10 +0100

    chore: disable local ssl for now

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
 import { imagetools } from 'vite-imagetools';
 
 export default defineConfig({
-    plugins: [sveltekit(), imagetools(), basicSsl()],
-    server: { https: true },
+    plugins: [sveltekit(), imagetools()],
 });
