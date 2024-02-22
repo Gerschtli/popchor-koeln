1bd895e5586abaf62abcb8ee4963b1808f1d22ef
Tobias Happ <tobias.happ@gmx.de>
2024-02-22 20:59:07 +0100

    fix: enable visual preview in editor

--- src/lib/storyblok/StoryblokInit.svelte
+++ src/lib/storyblok/StoryblokInit.svelte
@@ -2,11 +2,14 @@
     import type { PageStoryblok } from '$lib/component-types-storyblok';
     import { StoryblokComponent, useStoryblokBridge, type ISbStoryData } from '@storyblok/svelte';
     import { onMount } from 'svelte';
+    import { resolveRelations } from './setup';
 
     export let story: ISbStoryData<PageStoryblok>;
 
     onMount(() => {
-        useStoryblokBridge(story.id, (newStory) => (story = newStory));
+        useStoryblokBridge(story.id, (newStory) => (story = newStory), {
+            resolveRelations,
+        });
     });
 </script>
 
--- src/lib/storyblok/setup.ts
+++ src/lib/storyblok/setup.ts
@@ -4,6 +4,8 @@ import type { PageStoryblok } from '$lib/component-types-storyblok';
 import { components } from '$lib/components/storyblok';
 import { apiPlugin, storyblokInit, useStoryblokApi, type ISbStoryData } from '@storyblok/svelte';
 
+export const resolveRelations = ['gigs_reference.reference'];
+
 export function initStoryblokApi() {
     storyblokInit({
         accessToken: PUBLIC_STORYBLOK_ACCESS_TOKEN,
@@ -12,6 +14,7 @@ export function initStoryblokApi() {
             region: 'eu',
         },
         components,
+        bridge: true,
     });
 
     return useStoryblokApi();
@@ -20,7 +23,7 @@ export function initStoryblokApi() {
 export async function loadStory(storyblokApi: ReturnType<typeof initStoryblokApi>, story: string) {
     const dataStory = await storyblokApi.get(`cdn/stories/${story}`, {
         version: dev ? 'draft' : 'published',
-        resolve_relations: ['gigs_reference.reference'],
+        resolve_relations: resolveRelations,
     });
 
     return dataStory.data.story as ISbStoryData<PageStoryblok>;
