70bc5d15b6c10e02d85e1efff58c139b83d0ccae
Martin Stoecker <martinstoecker@gmx.de>
2025-02-22 15:21:59 +0100

    add picture element and sizes

--- src/lib/components/storyblok/richtext/ImageSlider.svelte
+++ src/lib/components/storyblok/richtext/ImageSlider.svelte
@@ -49,7 +49,30 @@
             data-height={dimensions.height}
             data-width={dimensions.width}
         >
-            <img class="max-h-[20rem]" loading="lazy" src={image.image.filename} alt={image.image.alt} />
+            <picture>
+                <source
+                    media="(max-width: 639px)"
+                    srcset="{image.image.filename}/m/558x0/filters:no_upscale() 1x, 
+                        {image.image.filename}/m/1116x0/filters:no_upscale() 2x"
+                    type="image/png"
+                />
+                <source
+                    media="(min-width: 640px) and (max-width: 767px)"
+                    srcset="{image.image.filename}/m/544x0/filters:no_upscale()"
+                    type="image/png"
+                />
+                <source
+                    media="(min-width: 768px)"
+                    srcset="{image.image.filename}/m/608x0/filters:no_upscale()"
+                    type="image/png"
+                />
+                <img
+                    class="max-h-[20rem]"
+                    loading="lazy"
+                    src="{image.image.filename}/m/608x0/filters:no_upscale()"
+                    alt={image.image.alt}
+                />
+            </picture>
         </a>
     </Slider>
 </div>
