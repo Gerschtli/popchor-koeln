e6aa2ea096dc80b7c72a9e941956d1d0fcb1a18c
Martin Stoecker <martinstoecker@gmx.de>
2025-03-08 13:57:15 +0100

    remove test page

--- src/routes/test/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import StoryblokInit from '$lib/storyblok/StoryblokInit.svelte';
-
-    export let data;
-</script>
-
-<StoryblokInit story={data.story} />
--- src/routes/test/+page.ts
+++
@@ -1,9 +0,0 @@
-import { loadStory } from '$lib/storyblok/setup';
-
-export async function load({ parent }) {
-    const { storyblokApi, storyblokCacheVersion } = await parent();
-
-    return {
-        story: await loadStory(storyblokApi, storyblokCacheVersion, 'test'),
-    };
-}
