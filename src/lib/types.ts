98647bc704efdf2b426553aa2e977e9dd7e6716f
Tobias Happ <tobias.happ@gmx.de>
2025-04-25 21:24:34 +0200

    chore: remove unused types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,17 +1,1 @@
-export enum FormId {
-    contact = 'contact',
-    newsletter = 'newsletter',
-}
-
 export type FormStatus = 'ready' | 'loading' | 'success' | 'error';
-
-export type Gig = {
-    date: Date;
-    title: string;
-    time: string;
-    place: string[];
-    price?: string;
-    description: string;
-    linkTickets?: string;
-    entryTime?: string;
-};
