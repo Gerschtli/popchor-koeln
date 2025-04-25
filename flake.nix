972590cb5a0e8c8991e11f8fd41faec429f10e7d
Tobias Happ <tobias.happ@gmx.de>
2025-04-25 20:58:21 +0200

    chore: support darwin in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,51 +2,64 @@
   description = "popchor-koeln";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      forEachSystem = nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-linux" ];
 
-      nodejs = pkgs.nodejs_20;
+      pkgsFor = forEachSystem (system: nixpkgs.legacyPackages.${system});
+
+      nodejsFor = forEachSystem (system: pkgsFor.${system}.nodejs_20);
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.netlify-cli
-          nodejs
-          nodejs.pkgs.pnpm
-        ];
-      };
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = pkgsFor.${system};
+            nodejs = nodejsFor.${system};
+          in
+          {
+            default = pkgs.mkShell {
+              packages = [
+                pkgs.netlify-cli
+                nodejs
+                nodejs.pkgs.pnpm
+              ];
+            };
+          });
 
-      checks.${system} =
-        let
-          inherit (pkgs.lib) getExe;
+      checks = forEachSystem
+        (system:
+          let
+            pkgs = pkgsFor.${system};
+            nodejs = nodejsFor.${system};
 
-          checker = name: description: package: versionCommand: pkgs.runCommand "check-${name}-version" { } ''
-            version_nix="${package.version}"
-            version_non_nix="$(${versionCommand})"
+            inherit (pkgs.lib) getExe versions;
 
-            echo "compare versions for ${description}"
-            echo "version_nix:     $version_nix"
-            echo "version_non_nix: $version_non_nix"
+            checker = name: description: package: versionCommand: pkgs.runCommand "check-${name}-version" { } ''
+              version_nix="${package.version}"
+              version_non_nix="$(${versionCommand})"
 
-            [[ "$version_nix" == "$version_non_nix" ]]
-            touch $out
-          '';
-        in
-        {
-          node = checker "node" "node version for netlify" nodejs ''
-            ${getExe pkgs.gnugrep} NODE_VERSION ${./netlify.toml} \
-              | ${getExe pkgs.gnused} -e 's,^.*"\(.*\)"$,\1,'
-          '';
+              echo "compare versions for ${description}"
+              echo "version_nix:     $version_nix"
+              echo "version_non_nix: $version_non_nix"
 
-          pnpm = checker "pnpm" "pnpm version in package.json" nodejs.pkgs.pnpm ''
-            ${getExe pkgs.jq} --raw-output '.packageManager' ${./package.json} \
-              | ${getExe pkgs.gnused} -e 's,pnpm@,,'
-          '';
-        };
+              [[ "$version_nix" == "$version_non_nix" ]]
+              touch $out
+            '';
+          in
+          {
+            node = checker "node" "node version for netlify" nodejs ''
+              ${getExe pkgs.gnugrep} NODE_VERSION ${./netlify.toml} \
+                | ${getExe pkgs.gnused} -e 's,^.*"\(.*\)"$,\1,'
+            '';
+
+            pnpm = checker "pnpm" "pnpm version in package.json" nodejs.pkgs.pnpm ''
+              ${getExe pkgs.jq} --raw-output '.packageManager' ${./package.json} \
+                | ${getExe pkgs.gnused} -e 's,^pnpm@,,'
+            '';
+          });
     };
 }
