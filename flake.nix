18d81660d2a26ba662ca856ab706427cb728a638
Tobias Happ <tobias.happ@gmx.de>
2026-05-21 22:12:49 +0200

    ci: fix pipeline

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
             default = pkgs.mkShell {
               packages = [
                 nodejs
-                #pkgs.netlify-cli
+                pkgs.netlify-cli
                 pkgs.pnpm
               ];
             };
