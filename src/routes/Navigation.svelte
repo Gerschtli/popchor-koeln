d6448b4cb5d776a7ed0de38e199b915989fb426d
Tobias Happ <tobias.happ@gmx.de>
2026-07-07 12:36:05 +0200

    fix: fix navigation close on navigating

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { goto } from '$app/navigation';
     import { page } from '$app/state';
     import { X } from '@lucide/svelte';
     import { fly } from 'svelte/transition';
@@ -27,8 +28,16 @@
         <div class="relative container mx-auto p-8">
             <ul class="flex flex-col gap-4 p-4">
                 {#each navigationItems as item (item.id)}
+                    {@const href = `/#${item.id}`}
                     <li>
-                        <a class="hover:text-accent-dark active:text-accent-dark text-lg" href="/#{item.id}">
+                        <a
+                            class="hover:text-accent-dark active:text-accent-dark text-lg"
+                            {href}
+                            onclick={(e) => {
+                                e.preventDefault();
+                                goto(href, { replaceState: true });
+                            }}
+                        >
                             {item.title}
                         </a>
                     </li>
